506cb27e3e9a6479b54ec03d35f3bd4ba2523e47
HanjoStudy <hanjo.odendaal@hotmail.com>
2018-03-16 10:25:48 +0200

    initial

---
+++ iemand/R/create_age.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @details This is an useless function
+#' @return
+#' @export
+#'
+#' @examples
+#' create_age()
+create_age <- function(){
+  age <- sample(1:100, size = 1)
+  return(age)
+}
